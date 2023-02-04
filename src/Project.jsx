7b38700ac906281c5cc5c513b7ca1eda4ead78de
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-04 23:38:22 +0800

    Use `target="_blank"`

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,11 +1,11 @@
 export default function Project(props) {
 	return (
 		<div className="project">
-			<a href={props.link}>
+			<a href={props.link} target="_blank">
 				<img src={props.image}/>
 			</a>
 			<div>
-				<a href={props.link}>
+				<a href={props.link} target="_blank">
 					<h2>{props.name} <span className="fas fa-external-link-alt"/></h2>
 				</a>
 				<span className="tag">{props.tags}</span>

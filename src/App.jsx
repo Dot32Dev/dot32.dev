67d99175c89ede5a8db573e887ab9760a2c3d08e
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-06 17:03:06 +0800

    Add header, use Sen font

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Project from './Project'
+import Header from './Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Header />
+
+      <br/>
+      <br/>
+      <br/>
+
       <h2>Featured</h2>
 
       <Project
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+// import './Header.css'
+
+export default function Project() {
+	return (
+		<div className="header-container">
+		<header>
+			<nav>
+				<ul>
+					<li><a href=""><img src="https://resonant-pastelito-dc9a23.netlify.app/white%20logo.png" style={{display: 'inline', margin: 0,width: '1em'}}/><p>Dot32</p></a></li>
+					<li><a href="">Projects</a></li>
+					<li><a href="">Tutorials</a></li>
+					<li><a href="">About</a></li>
+				</ul>
+			</nav>
+			<a href="" className="cta">Join my Discord</a>
+		</header>
+	</div>
+	)
+}

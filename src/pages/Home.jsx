b6441b6a0d70efafd32b1956ee07b909933b28e4
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-15 20:16:01 +0800

    Add gradient video to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 export default function ProjectList(props) {
 	return (
 		<div className="projectList">
-			<h2>Dot32</h2>
-			{/* <video autoplay="" playsinline="" muted="" loop="" preload="">
+			<video autoPlay muted playsInline loop>
 				<source src="/src/assets/gradientlogo.webm" />
-			</video> */}
+			</video>
 		</div>
 	)
 }

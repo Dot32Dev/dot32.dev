438a4d133c3e4e347abd5f46a75ea25c1f877ac4
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-16 16:35:24 +0800

    Move the tutorials out of the header

--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -14,6 +14,10 @@ export default function Tutorial(props) {
 
 	return (
 		<div className="projectList">
+			<br/>
+			<br/>
+			<br/>
+			
 			<ReactMarkdown>{markdown}</ReactMarkdown>
 		</div>
 	)

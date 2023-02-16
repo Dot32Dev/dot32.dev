20dbd864cce6974d4ba2c2257c8bcee223aeb1e4
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-16 14:06:58 +0800

    Add tutorials

--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -1,10 +1,20 @@
 import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
 
 export default function Tutorial(props) {
 	const { id } = useParams()
+	const [markdown, setMarkdown] = useState("");
+
+	useEffect(() => {
+		fetch(`/src/pages/tutorials/${id}.md`) 
+			.then((res) => res.text())
+			.then((text) => setMarkdown(text));
+	}, []);
+
 	return (
 		<div className="projectList">
-			<h1>{id}</h1>
+			<ReactMarkdown>{markdown}</ReactMarkdown>
 		</div>
 	)
 }

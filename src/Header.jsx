313c1f88e0c6d7807bd70ba3d65381aaae492aa9
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-21 10:55:06 +0800

    Remove about page for now

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,14 +7,10 @@ export default function Project() {
 		<header>
 			<nav>
 				<ul>
-					{/* <li><a href=""><img src="https://resonant-pastelito-dc9a23.netlify.app/white%20logo.png" style={{display: 'inline', margin: 0,width: '1em'}}/><p>Dot32</p></a></li>
-					<li><a href="">Projects</a></li>
-					<li><a href="">Tutorials</a></li>
-					<li><a href="">About</a></li> */}
 					<li><Link to="/"><img src="/white logo.png" style={{display: 'inline', margin: 0,width: '1em'}}/><p>Dot32</p></Link></li>
 					<li><Link to="/projects">Projects</Link></li>
 					<li><Link to="/tutorials">Tutorials</Link></li>
-					<li><Link to="/about">About</Link></li>
+					{/* <li><Link to="/about">About</Link></li> */}
 				</ul>
 			</nav>
 			<a href="" className="cta">Join my Discord</a>

57a90261b4988a4019681594df2484318f0a1188
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-21 10:36:38 +0800

    Animate in my logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import useDocumentTitle from './useDocumentTitle'
+import {useRef, useEffect} from 'react';
 
 export default function ProjectList(props) {
+	const ref = useRef(null);
 	useDocumentTitle('Dot32')
+
+	useEffect(() => {
+		// Get the video element
+		const video = ref.current;
+		// Add a class to the video when it is loaded
+		video.onloadeddata = function() { // first frame has loaded
+			video.classList.add("animate");
+		}
+	}, []);
+
 	return (
 		<div className="projectList">
-			<video autoPlay muted playsInline loop>
+			<video autoPlay muted playsInline loop ref={ref}>
 				<source src="/gradientlogo.webm" />
 			</video>
 		</div>

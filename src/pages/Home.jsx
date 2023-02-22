c1628ea3c4207909e67b80ffb73352fef2073f52
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-22 13:31:30 +0800

    Fix massive gap between video and text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,20 +6,26 @@ export default function ProjectList(props) {
 	useDocumentTitle('Dot32')
 
 	useEffect(() => {
+		// Get the video-container element
+		const video_container = ref.current;
 		// Get the video element
-		const video = ref.current;
-		// Add a class to the video when it is loaded
+		const video = video_container.querySelector("video");
+		// Add a class to the video-container when it is loaded
 		video.onloadeddata = function() { // first frame has loaded
-			video.classList.add("animate");
+			video_container.classList.add("animate");
 		}
 	}, []);
 
 	return (
 		<div className="projectList">
 			<p className="large-text">Hi there, I'm <span>Dot32</span></p>
-			<video autoPlay muted playsInline loop ref={ref}>
-				<source src="/gradientlogo.webm" />
-			</video>
+			<div className="video-container" ref={ref}>
+				<video autoPlay muted playsInline loop>
+					<source src="/gradientlogo.webm" />
+				</video>
+			</div>
+			<br/>
+			<br/>
 			<p className="large-text sub"><span>[ RUSTACIAN + WEBDEV ]</span></p>
 		</div>
 	)

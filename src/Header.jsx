69ad9f5856e1e808f051cc38307b344882e5fd72
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-06-25 14:03:04 +0800

    Fix animation direction when leaving tutorials

--- src/Header.jsx
+++ src/Header.jsx
@@ -41,28 +41,36 @@ const AnimatedLink = ({ to, children }) => {
 					// Get currently active page from the list of pages and if it is record the index
 					const activeIndex = pages.indexOf(window.location.pathname);
 					// If the active page is in the list of pages
-					if (activeIndex !== -1) {
-						// If the active page index is greater than the page index
-						if (activeIndex > index) {
+					// if (activeIndex !== -1) {
+					if (activeIndex == -1) {
+						// If the active page titles includes tutorial
+						if (window.location.pathname.includes('tutorial')) {
 							// Make the page slide in from the left
 							document.documentElement.classList.add('back-transition');
 						}
-
-						if (activeIndex !== index) {
-							// Start the transition
-							const transition = document.startViewTransition(() => {
-								navigate(to);
-							});
-
-							try {
-								await transition.finished;
-							} finally {
-								document.documentElement.classList.remove('back-transition');
-							}
-						}
-					} else {
-						navigate(to);
 					}
+
+					// If the active page index is greater than the page index
+					if (activeIndex > index) {
+						// Make the page slide in from the left
+						document.documentElement.classList.add('back-transition');
+					}
+
+					if (activeIndex !== index) {
+						// Start the transition
+						const transition = document.startViewTransition(() => {
+							navigate(to);
+						});
+
+						try {
+							await transition.finished;
+						} finally {
+							document.documentElement.classList.remove('back-transition');
+						}
+					}
+					// } else {
+					// 	navigate(to);
+					// }
 				}
 			} else {
 				navigate(to);

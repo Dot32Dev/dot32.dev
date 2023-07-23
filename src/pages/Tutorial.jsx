752cca76bdb02b550ce26df0497e0d2d8b357bb6
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-07-23 12:19:55 +0800

    Contents list functionality

--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -11,7 +11,8 @@ import hljs from "highlight.js";
 export default function Tutorial(props) {
 	const { id } = useParams()
 	const [markdown, setMarkdown] = useState("");
-	const isReady = markdown !== "";
+	const [h2Headings, setH2Headings] = useState([]);
+	// const isReady = markdown !== "";
 
 	// Set title to contents of first h1
 	useDocumentTitle(markdown.split("\n")[0].replace("# ", ""))
@@ -23,14 +24,28 @@ export default function Tutorial(props) {
 			setMarkdown(text);
 			console.log("markdown fetch")
 		});
-	}, []);
+	}, [markdown]);
 		
 	useEffect(() => {
 		hljs.highlightAll();
-		console.log("highlighted")
-	});
+		// console.log("highlighted")
+	}, [markdown]);
 
+	useEffect(() => {
+		// Get all h2 headings
+		const h2Elements = document.querySelectorAll("h2");
+		// Set the id of each h2 element to its text content
+		h2Elements.forEach((heading) => {
+			const id = heading.textContent.toLowerCase().replace(/\s+/g, '-');
+			heading.id = id;
+		});
 
+		// Extract the text content of each h2 element and store in an array
+		const headings = Array.from(h2Elements).map((heading) => heading.textContent);
+		// Update the state variable with the extracted headings
+		setH2Headings(headings);
+		console.log(headings)
+	}, [markdown]);
 
 	return (
 		<div className="tutorial">
@@ -39,6 +54,30 @@ export default function Tutorial(props) {
 			<br/>
 			
 			<ReactMarkdown rehypePlugins={[rehypeRaw]}>{markdown}</ReactMarkdown>
+
+			{/* <ReactMarkdown
+				// children={markdown}
+				// renderers={{
+				// heading: ({ level, children }) => {
+				// 	// Generate a unique id based on the heading content
+				// 	const id = children.map((child) => child.props.value.toLowerCase()).join('-');
+				// 	// Adjust the 'level' prop to create nested headings correctly
+				// 	const Tag = `h${level + 1}`;
+				// 	return <Tag id={id}>{children}</Tag>;
+				// },
+				// }}
+				components={{
+					heading: ({ level, children }) => {
+						// Generate a unique id based on the heading content
+						const id = children.map((child) => child.props.value.toLowerCase()).join('-');
+						console.log("i was here")
+						// Adjust the 'level' prop to create nested headings correctly
+						const Tag = `h${level + 1}`;
+						return <Tag id={id}>{children}</Tag>;
+					}
+				}}
+			>markdown</ReactMarkdown> */}
+
 			{/* {isReady ? (
 				<ReactMarkdown rehypePlugins={[rehypeRaw]}>{markdown}</ReactMarkdown>
 			) : (
@@ -49,6 +88,19 @@ export default function Tutorial(props) {
 			{/* <hr/>
 
 			<p>Have any questions? Join the <a className="cta" href="https://discord.gg/Pswb8khdgQ">Discord server</a></p> */}
+
+			{markdown !== ""? (
+				<div>
+					<h2>Contents:</h2>
+					<ul>
+						{h2Headings.map((heading, index) => (
+						<li key={index}>
+							<a href={`#${heading.toLowerCase().replace(/\s+/g, '-')}`}>{heading}</a>
+						</li>
+						))}
+					</ul>
+				</div>
+			): null}
 		</div>
 	)
 }

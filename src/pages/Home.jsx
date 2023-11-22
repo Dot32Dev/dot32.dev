db20fe0b490ee65a8f59c5eb01429cc8d61352c7
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-11-22 16:00:39 +0800

    YOOO THING WORK ON SAFARI AND IPHONE AND CHROME

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,8 +39,11 @@ export default function ProjectList(props) {
 
 			<p className="large-text top" ref={(el) => (animate.current[0] = el)}>Hi there, I'm <span>Dot32</span></p>
 			<div className="video-container" ref={vid}>
+				{/* <video autoPlay muted playsInline loop>
+					<source src="/gradientlogo_VP9.webm" />
+				</video> */}
 				<video autoPlay muted playsInline loop>
-					<source src="/gradientlogo.webm" />
+					<source src="/gradientlogo_H.265.mp4" />
 				</video>
 				{/* <video autoPlay muted playsInline loop>
 					<source src="/gradientlogo.webm" />
